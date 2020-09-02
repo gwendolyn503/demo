2876c560ebb3decc9369f5d8e94133b9b660bc48
gwendolyn503 <2432284671@qq.com>
2020-09-02 18:03:44 +0800

    init

---
+++ 1.react/day01/react-app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+
+function App() {
+  return (
+    <Fragment>
+      <div className="App">
+        <h1>this is a react</h1>
+      </div>
+      <div className="App22">
+        <h1>2222</h1>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
